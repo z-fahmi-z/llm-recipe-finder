bd08a80623b8a9dbf9dc3fbc5852e39e2383bdd2
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2025-05-11 11:35:19 +0200

    Add frontend components: --> card_container --> card

---
+++ frontend/components.py
@@ -0,0 +1,16 @@
+card_container = """
+<div id="main-card-container">
+    {cards}
+</div>
+"""
+
+card = """
+<div class="recipe-card">
+    <div class="recipe-image">
+        Image
+    </div>
+    <div class="recipe-content">
+        <div class="recipe-title">{title}</div>
+    </div>
+</div>
+"""
