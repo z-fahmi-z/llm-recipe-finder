4530c3a1a89dc915e64f8a4be93bc62fd0a25a6f
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2025-05-16 00:35:17 +0200

    Define popupOverlay component

--- frontend/components.py
+++ frontend/components.py
@@ -2,6 +2,17 @@ card_container = """
 <div id="main-card-container">
     {cards}
 </div>
+<div id="popupOverlay" class="popup-overlay">
+    <div class="popup-content">
+        <button id="closePopup" class="close-button">&times;</button>
+        <div class="popup-columns">
+            <div class="popup-left">
+            </div>
+            <div class="popup-right">
+            </div>
+        </div>
+    </div>
+</div>
 """
 
 card = """
